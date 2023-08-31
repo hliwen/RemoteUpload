43a3d994e3d50b3ca056502b5af3f2580f2b6c2f
hlw <hlw@dydev.com>
2023-08-31 18:03:14 +0800

    添加U盘操作测试代码

--- app/src/main/java/com/example/nextclouddemo/utils/Utils.java
+++ app/src/main/java/com/example/nextclouddemo/utils/Utils.java
@@ -16,13 +16,18 @@ import com.example.nextclouddemo.model.UploadFileModel;
 
 import androidx.core.content.PermissionChecker;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Utils {
@@ -180,6 +185,158 @@ public class Utils {
             }
         }
         dir.delete();// 删除目录本身
+    }
+
+
+    public static List<String> getUSBDeviceName() {
+        List<String> usbDevices = new ArrayList<>();
+        try {
+            Process process = Runtime.getRuntime().exec("cat /proc/mounts");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.contains("/mnt/media_rw/")) {
+                    String usbDevice = line.substring(line.indexOf("/dev/"));
+                    Log.e(TAG, "getUSBDeviceName: usbDeviceName = " + usbDevice);
+                    if (usbDevice != null) {
+                        usbDevices.add(usbDevice);
+                    }
+                }
+            }
+            reader.close();
+            process.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return usbDevices;
+    }
+
+
+    public static void formatUSB(String usbDeviceName) {
+        try {
+            // 获取 root 权限
+            Process suProcess = Runtime.getRuntime().exec("su");
+            DataOutputStream os = new DataOutputStream(suProcess.getOutputStream());
+            // 获取 U 盘设备名称，可以通过命令行查看
+            String usbDevice = "/dev/" + usbDeviceName;  // 替换为你的 U 盘设备名称
+            // 卸载 U 盘
+            os.writeBytes("umount " + usbDevice + "\n");
+
+            // 格式化 U 盘为 ext4 文件系统
+            //os.writeBytes("mkfs.ext4 " + usbDevice + "\n");
+
+            //格式化为 FAT32 文件系统
+            os.writeBytes("mkfs.vfat -F 32 " + usbDevice + "\n");
+
+            //格式化为 NTFS 文件系统（需要设备支持）
+            // os.writeBytes("mkfs.ntfs " + usbDevice + "\n");
+
+            // 关闭输出流和进程
+            os.writeBytes("exit\n");
+            os.flush();
+            os.close();
+            suProcess.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public static void test( ) {
+        String usbDevicePath = "/dev/sdX1";  // 替换为你的 U 盘设备路径
+        String mountPoint = "/mnt/usb";  // 替换为你的挂载点路径
+
+        // 检查是否已经挂载
+        if (!isMounted(mountPoint)) {
+            // 挂载 U 盘
+            if (mountUSB(usbDevicePath, mountPoint)) {
+                System.out.println("USB drive mounted.");
+            } else {
+                System.out.println("Failed to mount USB drive.");
+                return;
+            }
+        }
+
+        // 在挂载点下读取文件列表
+        listFiles(mountPoint);
+
+        // 卸载 U 盘
+        unmountUSB(mountPoint);
+    }
+
+    public static boolean isMounted(String path) {
+        try {
+            Process process = Runtime.getRuntime().exec("mount");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                if (line.contains(path)) {
+                    reader.close();
+                    process.waitFor();
+                    return true;
+                }
+            }
+
+            reader.close();
+            process.waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public static boolean mountUSB(String usbDevicePath, String mountPoint) {
+        try {
+            Process process = Runtime.getRuntime().exec("su");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder command = new StringBuilder();
+            command.append("mount -t vfat ").append(usbDevicePath).append(" ").append(mountPoint);
+
+            DataOutputStream os = new DataOutputStream(process.getOutputStream());
+            os.writeBytes(command.toString());
+            os.flush();
+            os.close();
+
+            process.waitFor();
+            reader.close();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void listFiles(String path) {
+        File directory = new File(path);
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                System.out.println(file.getName());
+            }
+        }
+    }
+
+    public static void unmountUSB(String mountPoint) {
+        try {
+            Process process = Runtime.getRuntime().exec("su");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder command = new StringBuilder();
+            command.append("umount ").append(mountPoint);
+
+            DataOutputStream os = new DataOutputStream(process.getOutputStream());
+            os.writeBytes(command.toString());
+            os.flush();
+            os.close();
+
+            process.waitFor();
+            reader.close();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
